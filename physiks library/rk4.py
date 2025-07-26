49e12a0bbcdb0865b3a43f61e5db60fbc58d8605
Captos <alphax1273@gamil.com>
2025-07-26 17:52:37 +0100

    Update rk4.py

--- physiks library/rk4.py
+++ physiks library/rk4.py
@@ -1,2 +1,17 @@
-def rk4_step(function, ):
-    pass
+def rk4_step(function, state, dt, t):
+
+    k1 = function(state, t)
+    k2 = function(state * t + k1 * (dt/2), t + dt/2 )
+    k3 = function(state * t + k2 * (dt/2), t + dt/2 )
+    k4 = function(state * t + k3 * dt, t + dt)
+    
+    return state * time + dt*(k1/6 + k2/3 + k3/3 + k4/6)
+
+def rk4_step_vector(function, state, dt, t):
+    k1 = function(state, t)
+    k2 = function(state * t + k1 * (dt/2), t + dt/2 )
+    k3 = function(state * t + k2 * (dt/2), t + dt/2 )
+    k4 = function(state * t + k3 * dt, t + dt)
+    
+    return state * time + dt*(k1/6 + k2/3 + k3/3 + k4/6)
+
