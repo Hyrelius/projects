6b692e4ce37b5b476e60537d436d98ead6954b8e
Hyrelius <alphax1273@gmail.com>
2025-08-06 22:47:46 +0100

    further testing of the ball object

--- quiktest.py
+++ quiktest.py
@@ -1,12 +1,18 @@
 #each ball contains its state, which is an array of its positions and velocities
 class ball:
-     def __innit__(self, state):
+    def __innit__(self, state):
         self.state = state
 
     def get_position(self):
         n = int(len(self.state) / 2)
-        return state[:n] 
+        return self.state[:n] 
 
     def get_velocity(self):
         n = int(len(self.state) / 2)
-        return state[n:] 
+        return self.state[n:] 
+
+
+state = [1,2,5,6]
+baller = ball()
+baller.state = state
+print(baller.get_position())
