40acaf0787e2b18cd86c97b5c5d05f1b6295b447
Hyrelius <alphax1273@gmail.com>
2025-08-07 17:51:47 +0100

    basic diffrential equation

--- physiks_library/rk4.py
+++ physiks_library/rk4.py
@@ -4,13 +4,12 @@ diffrential equation is the function for each physical system
 dt is the time step
 t is the time
 """
-def rk4_step():
-    #diffrential_equation, state, dt, t):
+def rk4_step(diffrential_equation, state, dt, t):
 
-    #k1 = diffrential_equation(state, t)
-    #k2 = diffrential_equation(state * t + k1 * (dt/2), t + dt/2 )
-    #k3 = diffrential_equation(state * t + k2 * (dt/2), t + dt/2 )
-    #k4 = diffrential_equation(state * t + k3 * dt, t + dt)
-    print("imported")
-    #return state * t + dt*(k1/6 + k2/3 + k3/3 + k4/6)
+    k1 = diffrential_equation(state, t)
+    k2 = diffrential_equation(state * t + k1 * (dt/2), t + dt/2 )
+    k3 = diffrential_equation(state * t + k2 * (dt/2), t + dt/2 )
+    k4 = diffrential_equation(state * t + k3 * dt, t + dt)
+    
+    return state * t + dt*(k1/6 + k2/3 + k3/3 + k4/6)
 
