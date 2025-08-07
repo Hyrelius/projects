d4460ff309d61e9fdbaf713821e3aa2f4b01cba3
Hyrelius <alphax1273@gmail.com>
2025-08-07 17:49:36 +0100

    sorted out import problem

--- quiktest.py
+++ quiktest.py
@@ -1,5 +1,12 @@
 #each ball contains its state, which is an array of its positions and velocities
-from physics_library.rk4 import rk4
+#import sys
+#import os
+#import numpy as np 
+
+#sys.path.append(os.path.join(os.path.dirname(__file__), 'physiks_library'))
+
+from physiks_library.rk4 import *
+
 class ball:
     def __innit__(self, state):
         self.state = state
@@ -12,7 +19,7 @@ class ball:
         n = int(len(self.state) / 2)
         return self.state[n:] 
 
-
+rk4_step()
 state = [1,2,5,6]
 baller = ball()
 baller.state = state
