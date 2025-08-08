dfdedd7e67a15646cefdf57db43242b52f87818b
Hyrelius <alphax1273@gmail.com>
2025-08-08 21:07:22 +0100

    no clue honestly its been a while

--- physics/physlibrary/integrators/euler.py
+++ physics/physlibrary/integrators/euler.py
@@ -1,3 +1,4 @@
-def euler():
-    #euler here
-    pass
+def euler_step(differential_equation, state, dt, t):
+    return state + dt * differential_equation(state, t)
+    #comically simple
+    #comically incorrect
