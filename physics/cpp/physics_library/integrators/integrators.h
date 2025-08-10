dbbd4e9a94b17b6617c71ba4742f02879f371447
Captos <alphax1273@gamil.com>
2025-08-10 21:58:22 +0100

    huh

--- physics/cpp/physics_library/integrators/integrators.h
+++ physics/cpp/physics_library/integrators/integrators.h
@@ -3,7 +3,7 @@
 #include "state.h"
 
 
-State euler_step();
+State euler_step(State (*f)(State state, double), State state, double dt, double t);
 State velocity_verlet_step();
 State rk4_step();
 #endif
